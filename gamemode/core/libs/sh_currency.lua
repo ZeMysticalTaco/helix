71575a4fd15c4509b40c7508353c510e88a0dbdd
ZeMysticalTaco <zemysticaltaco@gmail.com>
2021-05-23 04:42:50 -0400

    library docs: ix.currency
    Added usage example for ix.currency.Set, added docs for char metafuncs.

--- gamemode/core/libs/sh_currency.lua
+++ gamemode/core/libs/sh_currency.lua
@@ -9,11 +9,15 @@ ix.currency.plural = ix.currency.plural or "dollars"
 ix.currency.model = ix.currency.model or "models/props_lab/box01a.mdl"
 
 --- Sets the currency type.
+-- This is how you would set the currency for your Schema.
 -- @realm shared
 -- @string symbol The symbol of the currency.
 -- @string singular The name of the currency in it's singular form.
 -- @string plural The name of the currency in it's plural form.
 -- @string model The model of the currency entity.
+-- @usage
+-- ix.currency.Set('₽', 'Monopoly Dollar', 'Monopoly Dollars', 'models/headcrab.mdl')
+-- -- Sets the currency to `Monopoly Dollar(s)`, using the symbol representing the Russian Ruble and the Fast Headcrab model.
 function ix.currency.Set(symbol, singular, plural, model)
 	ix.currency.symbol = symbol
 	ix.currency.singular = singular
@@ -77,6 +81,15 @@ end
 do
 	local character = ix.meta.character
 
+	--- Character currency methods
+	-- @classmod Character
+
+	--- Whether or not this `Character` has less than `amount` of money.
+	-- [The implementation in ixhl2rp's Vending Machine.](https://github.com/NebulousCloud/helix-hl2rp/blob/3c8d7f1a37489cddd773eb76869ff8abb4a33a8b/entities/entities/ix_vendingmachine.lua\#L136)
+	-- @realm shared
+	-- @number amount The amount to check for.
+	-- @treturn bool Whether or not the character has enough money.
+	-- @see ix.currency
 	function character:HasMoney(amount)
 		if (amount < 0) then
 			print("Negative Money Check Received.")
@@ -85,6 +98,16 @@ do
 		return self:GetMoney() >= amount
 	end
 
+	--- Give an amount of money to a `Character`.
+	-- The difference between `GiveMoney`, `TakeMoney` and `SetMoney` is that you must log `SetMoney` manually.
+	-- Is that you must log `SetMoney` manually.
+	-- @realm shared
+	-- @number amount The amount to give.
+	-- @bool bNoLog Whether or not to log this action.
+	-- @treturn true Always returns true.
+	-- @see ix.currency
+	-- @see SetMoney
+	-- @see TakeMoney
 	function character:GiveMoney(amount, bNoLog)
 		amount = math.abs(amount)
 
@@ -97,6 +120,16 @@ do
 		return true
 	end
 
+	--- Take an amount of money from a `Character`.
+	-- The difference between `GiveMoney`, `TakeMoney` and `SetMoney` is that you must log `SetMoney` manually.
+	-- Is that you must log `SetMoney` manually.
+	-- @realm shared
+	-- @number amount The amount to take
+	-- @bool bNoLog Whether or not to log this action.
+	-- @treturn true Always returns true.
+	-- @see ix.currency
+	-- @see SetMoney
+	-- @see GiveMoney
 	function character:TakeMoney(amount, bNoLog)
 		amount = math.abs(amount)
 
